8ef027309270d49298d23140e5505a5a7e501ad2
Joiner2003 <Joineredh@gmail.com>
2021-10-25 21:31:34 -0500

    Correción de importaciones

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
-import bcrypt
+
 
 app = Flask(__name__)
 app.secret_key="BurberryGroup"
